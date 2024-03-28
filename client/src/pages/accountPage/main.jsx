eb7d16d9742ee89ea6bd9fcb4c0a812c3c44da6b
Владислав Вибираний <31660416+BananaJam@users.noreply.github.com>
2024-03-28 23:21:02 +0200

    added tabs and toggle components, created userbar and settings tab on user page

---
+++ client/src/pages/accountPage/main.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { 
+        Tabs, 
+        TabTriggers, 
+        TabTrigger,
+        TabsContent,
+        TabContent,
+    } from '../../components/tabs';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChartSimple, faUtensils, faBowlRice, faGear } from '@fortawesome/free-solid-svg-icons';
+
+import UserBar from './userbar';
+import Settings from './settings';
+
+export default function AccountPage() {
+    return (
+        <div className="">
+            <h1>Account Page</h1>
+            <Tabs defaultTab="dashboard">
+                <TabTriggers>
+                    <TabTrigger tab="dashboard"><FontAwesomeIcon icon={faChartSimple} className='m-1' />Dashboard</TabTrigger>
+                    <TabTrigger tab="recipes"><FontAwesomeIcon icon={faUtensils} className='m-1' />Recipes</TabTrigger>
+                    <TabTrigger tab="ingredients"><FontAwesomeIcon icon={faBowlRice} className='m-1' />Ingredients</TabTrigger>
+                    <TabTrigger tab="settings"><FontAwesomeIcon icon={faGear} className='m-1' />Settings</TabTrigger>
+                </TabTriggers>
+                <TabsContent>
+                    <UserBar />
+                    <TabContent tab="dashboard">
+                        
+                    </TabContent>
+                    <TabContent tab="recipes">
+                        <h2>Recipes</h2>
+                        <p>Recipes content</p>
+                    </TabContent>
+                    <TabContent tab="ingredients">
+                        <h2>Ingredients</h2>
+                        <p>Ingredients content</p>
+                    </TabContent>
+                    <TabContent tab="settings">
+                        <Settings />
+                    </TabContent>
+                </TabsContent>
+            </Tabs>
+        </div>
+    )
+}
