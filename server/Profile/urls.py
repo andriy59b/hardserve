461176615f58f49c4911308737d27d44ee941409
Thundem <vovkandrij7@gmail.com>
2024-04-08 23:27:18 +0300

    added urls for removing, adding and displaying favorite recipes

--- server/Profile/urls.py
+++ server/Profile/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('favourites/', FavouriteProductsView.as_view(), name='favorite-products'),
-    path('favourites/recipes', FavouriteRecipesView.as_view(), name='favorite-recipes'),
+    path('favorites/', FavouriteProductsView.as_view(), name='favorite-products'),
+    path('favorites/recipes', FavouriteRecipesView.as_view(), name='favorite-recipes'),
     path('favorites/<int:product_id>/remove/', RemoveFavouriteProductsView.as_view(), name='remove-favorite-product'),
-    path('favorites/<int:recipe_id>/remove/', RemoveFavouriteRecipesView.as_view(), name='remove-favorite-recipe'),
+    path('favorites/recipes/<int:recipe_id>/remove/', RemoveFavouriteRecipesView.as_view(), name='remove-favorite-recipe'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
