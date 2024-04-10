169ad93f638222a59da5586d2447f01a9a1ee4c0
mykyta07 <mykyta@gmail.com>
2024-04-10 21:05:39 +0300

    updated serializers for returning all info about favorite product

--- server/Profile/serializers.py
+++ server/Profile/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Profile, Favorite
+from .models import *
 
 class ProfileSerializer(serializers.ModelSerializer):
     username = serializers.SerializerMethodField()
@@ -14,7 +14,36 @@ class ProfileSerializer(serializers.ModelSerializer):
     def get_email(self, obj):
         return obj.user.email
 
+
+class ProductSerializer(serializers.ModelSerializer):
+    glycemic_index = serializers.SerializerMethodField()
+    proteins = serializers.SerializerMethodField()
+    carbs = serializers.SerializerMethodField()
+    fats = serializers.SerializerMethodField()
+    calories = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Product
+        fields = ('product_id', 'name', 'category', 'short_description', 'image', 'glycemic_index', 'proteins', 'carbs', 'fats', 'calories')
+
+    def get_glycemic_index(self, obj):
+        return obj.glycemic_index
+
+    def get_proteins(self, obj):
+        return obj.proteins
+
+    def get_carbs(self, obj):
+        return obj.carbs
+
+    def get_fats(self, obj):
+        return obj.fats
+
+    def get_calories(self, obj):
+        return obj.calories
+
 class FavoriteProductSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+
     class Meta:
         model = Favorite
         fields = ['product']
