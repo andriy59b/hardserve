714f954e7e5bae10f3003f59ca4efc0f99030a00
Nieszczesliwy <zborivskyiworkingspace@gmail.com>
2024-05-09 21:05:35 +0300

    Check

--- server/ration/models.py
+++ server/ration/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from Profile.models import Favorite, UserWeightHistory
+from products.models import Product
+from recipes.models import Recipe
+from django.conf import settings
 
-# Create your models here.
+user = get_user_model()
+class Profile(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile_gold')
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    recipes = models.ForeignKey(Recipe, on_delete=models.CASCADE)    
+    
--- server/ration/views.py
+++ server/ration/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from openai import OpenAI
+from .models import *
 
 def query_openai(request):
     if request.method == 'GET':
@@ -14,7 +15,7 @@ def query_openai(request):
         completion = client.chat.completions.create(
             model="meta/llama3-70b-instruct",
             messages=[
-                {"role":"system","content":"You are a helpful assistant that specializes in discussing products, rations, ingredients, and related topics. You can provide information, answer questions, and offer suggestions. You do not answer on other topics. "},
+                {"role":"system","content":"You are a helpful assistant that specializes in discussing products, rations, ingredients, and related topics. You can provide information, answer questions, and offer suggestions. You do not answer on other topics. Here`s more inforamtion about user - {user}, he`s favorite products are {products}, favorite recipes are {recipes} and his weight history is {weight_history}"},
                 {"role":"user","content": prompt}
             ],
             temperature=0.5,
