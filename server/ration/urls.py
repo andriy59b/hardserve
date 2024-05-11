b5ecac2af09bd35cec68f518ba1e677b739b7cc9
mykyta07 <mykytanyk@gmail.com>
2024-05-11 23:27:05 +0300

    added urls for additing user's basic ration and remove it

--- server/ration/urls.py
+++ server/ration/urls.py
@@ -4,4 +4,7 @@ from .views import *
 urlpatterns = [
     path('query-openai/', QueryOpenAIView.as_view(), name='QueryOpenAIView'),
     path('basic', RationBasicView.as_view(), name='basic'),
+    path('basic/profile/<int:ration_id>/', ProfileBasicView.as_view(), name='profile_basic_add'),
+    path('basic/profile/', UserBasicRationsView.as_view(), name='profile_basic'),
+
 ]
