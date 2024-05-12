01bb30f4d97c36366b6cd9af3f3d0d2f4b3998b4
Thundem <vovkandrij7@gmail.com>
2024-05-12 19:33:01 +0300

    added 4 and 5 rations to basic plan

--- server/ration/management/commands/load_rations.py
+++ server/ration/management/commands/load_rations.py
@@ -7,8 +7,8 @@ class Command(BaseCommand):
     ration1, created = Ration_Basic.objects.get_or_create(name='Energy Boost')
     ration2, created = Ration_Basic.objects.get_or_create(name='Balanced Diet')
     ration3, created = Ration_Basic.objects.get_or_create(name='Fitness Challenge')
-    # ration4, created = Ration_Basic.objects.get_or_create(name='Healthy Eating')
-    # ration5, created = Ration_Basic.objects.get_or_create(name='Dynamic Endurance')
+    ration4, created = Ration_Basic.objects.get_or_create(name='Healthy Eating')
+    ration5, created = Ration_Basic.objects.get_or_create(name='Dynamic Endurance')
 
     try:
         #Раціон 1
@@ -117,63 +117,70 @@ class Command(BaseCommand):
     except Exception as e:
         print(f"Помилка при додаванні раціону 3: {e}")
 
-    # #Раціон 4
-    # recipe1 = Recipe.objects.get(id=) # breakfast 4
-    # recipe2 = Recipe.objects.get(id=) # breakfast 4
-    # recipe3 = Recipe.objects.get(id=) # breakfast 4
+    try:
+        # #Раціон 4
+        recipe1 = Recipe.objects.get(id=133) # breakfast 4
+        recipe2 = Recipe.objects.get(id=82) # breakfast 4
+        recipe3 = Recipe.objects.get(id=1360) # breakfast 4
 
-    # recipe4 = Recipe.objects.get(id=) # lunch 4
-    # recipe5 = Recipe.objects.get(id=) # lunch 4
-    # recipe6 = Recipe.objects.get(id=) # lunch 4
+        recipe4 = Recipe.objects.get(id=110) # lunch 4
+        recipe5 = Recipe.objects.get(id=30) # lunch 4
+        recipe6 = Recipe.objects.get(id=428) # lunch 4
 
-    # recipe7 = Recipe.objects.get(id=) # snack 4
-    # recipe8 = Recipe.objects.get(id=) # snack 4
-    # recipe9 = Recipe.objects.get(id=) # snack 4
+        recipe7 = Recipe.objects.get(id=63) # snack 4
+        recipe8 = Recipe.objects.get(id=165) # snack 4
+        recipe9 = Recipe.objects.get(id=151) # snack 4
 
-    # recipe10 = Recipe.objects.get(id=) # dinner 4 
-    # recipe11 = Recipe.objects.get(id=) # dinner 4
-    # recipe12 = Recipe.objects.get(id=) # dinner 4
-
-
-    # component4 = Ration_Basic_Components.objects.create(ration=ration4, meal_time='breakfast')
-    # component4.recipe.add(recipe1, recipe2, recipe3)
-
-    # component4 = Ration_Basic_Components.objects.create(ration=ration4, meal_time='lunch')
-    # component4.recipe.add(recipe4, recipe5, recipe6)
-
-    # component4 = Ration_Basic_Components.objects.create(ration=ration4, meal_time='snack')
-    # component4.recipe.add(recipe7, recipe8, recipe9)
-
-    # component4 = Ration_Basic_Components.objects.create(ration=ration4, meal_time='dinner')
-    # component4.recipe.add(recipe10, recipe11, recipe12)
+        recipe10 = Recipe.objects.get(id=40) # dinner 4 
+        recipe11 = Recipe.objects.get(id=128) # dinner 4
+        recipe12 = Recipe.objects.get(id=148) # dinner 4
 
 
-    # #Раціон 5
-    # recipe1 = Recipe.objects.get(id=) # breakfast 5
-    # recipe2 = Recipe.objects.get(id=) # breakfast 5
-    # recipe3 = Recipe.objects.get(id=) # breakfast 5
+        component4 = Ration_Basic_Components.objects.create(ration=ration4, meal_time='breakfast')
+        component4.recipe.add(recipe1, recipe2, recipe3)
 
-    # recipe4 = Recipe.objects.get(id=) # lunch 5
-    # recipe5 = Recipe.objects.get(id=) # lunch 5
-    # recipe6 = Recipe.objects.get(id=) # lunch 5
+        component4 = Ration_Basic_Components.objects.create(ration=ration4, meal_time='lunch')
+        component4.recipe.add(recipe4, recipe5, recipe6)
 
-    # recipe7 = Recipe.objects.get(id=) # snack 5
-    # recipe8 = Recipe.objects.get(id=) # snack 5
-    # recipe9 = Recipe.objects.get(id=) # snack 5
+        component4 = Ration_Basic_Components.objects.create(ration=ration4, meal_time='snack')
+        component4.recipe.add(recipe7, recipe8, recipe9)
 
-    # recipe10 = Recipe.objects.get(id=) # dinner 5 
-    # recipe11 = Recipe.objects.get(id=) # dinner 5
-    # recipe12 = Recipe.objects.get(id=) # dinner 5
+        component4 = Ration_Basic_Components.objects.create(ration=ration4, meal_time='dinner')
+        component4.recipe.add(recipe10, recipe11, recipe12)
+        print("Раціон 4 успішно додано.")
+    except Exception as e:
+        print(f"Помилка при додаванні раціону 4: {e}")
+
+    try:
+        # #Раціон 5
+        recipe1 = Recipe.objects.get(id=587) # breakfast 5
+        recipe2 = Recipe.objects.get(id=29) # breakfast 5
+        recipe3 = Recipe.objects.get(id=24) # breakfast 5
+
+        recipe4 = Recipe.objects.get(id=213) # lunch 5
+        recipe5 = Recipe.objects.get(id=154) # lunch 5
+        recipe6 = Recipe.objects.get(id=683) # lunch 5
+
+        recipe7 = Recipe.objects.get(id=507) # snack 5
+        recipe8 = Recipe.objects.get(id=515) # snack 5
+        recipe9 = Recipe.objects.get(id=73) # snack 5
+
+        recipe10 = Recipe.objects.get(id=806) # dinner 5 
+        recipe11 = Recipe.objects.get(id=1065) # dinner 5
+        recipe12 = Recipe.objects.get(id=164) # dinner 5
 
 
-    # component5 = Ration_Basic_Components.objects.create(ration=ration5, meal_time='breakfast')
-    # component5.recipe.add(recipe1, recipe2, recipe3)
+        component5 = Ration_Basic_Components.objects.create(ration=ration5, meal_time='breakfast')
+        component5.recipe.add(recipe1, recipe2, recipe3)
 
-    # component5 = Ration_Basic_Components.objects.create(ration=ration5, meal_time='lunch')
-    # component5.recipe.add(recipe4, recipe5, recipe6)
+        component5 = Ration_Basic_Components.objects.create(ration=ration5, meal_time='lunch')
+        component5.recipe.add(recipe4, recipe5, recipe6)
 
-    # component5 = Ration_Basic_Components.objects.create(ration=ration5, meal_time='snack')
-    # component5.recipe.add(recipe7, recipe8, recipe9)
+        component5 = Ration_Basic_Components.objects.create(ration=ration5, meal_time='snack')
+        component5.recipe.add(recipe7, recipe8, recipe9)
 
-    # component5 = Ration_Basic_Components.objects.create(ration=ration5, meal_time='dinner')
-    # component5.recipe.add(recipe10, recipe11, recipe12)
+        component5 = Ration_Basic_Components.objects.create(ration=ration5, meal_time='dinner')
+        component5.recipe.add(recipe10, recipe11, recipe12)
+        print("Раціон 5 успішно додано.")
+    except Exception as e:
+        print(f"Помилка при додаванні раціону 5: {e}")
